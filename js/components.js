0c2cf3b049b21a9cfad201f35b4bb4a7725ecb1c
felipesssantos <felipesssantos.ti@gmail.com>
2026-06-06 00:34:02 -0300

    Criação do site intitucional

--- js/components.js
+++ js/components.js
@@ -2,7 +2,10 @@ document.addEventListener("DOMContentLoaded", function() {
     // Carregar Header
     const headerPlaceholder = document.getElementById('header-placeholder');
     if (headerPlaceholder) {
-        fetch('/components/header.html')
+        const isInstitucional = headerPlaceholder.getAttribute('data-header-type') === 'institucional';
+        const headerUrl = isInstitucional ? '/components/header-institucional.html' : '/components/header.html';
+        
+        fetch(headerUrl)
             .then(response => {
                 if (!response.ok) throw new Error("Erro ao carregar o header");
                 return response.text();
